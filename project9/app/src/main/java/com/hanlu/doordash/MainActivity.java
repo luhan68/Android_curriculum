f6fa2d41f48455493ab311fb0ccf27b672c0625c
Lu Han <luhan@umail.ucsb.edu>
2018-08-20 10:58:18 +0800

    exercise

---
+++ project9/app/src/main/java/com/hanlu/doordash/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hanlu.doordash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<Food> foods;
+    private ArrayAdapter<Food> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list_view);
+        foods = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,foods);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+    }
+}
